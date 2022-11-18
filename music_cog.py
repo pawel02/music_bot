f28e5697cb446db252d3d322122331ef94364a72
Ignacio Romang <ignacio21496@gmail.com>
2022-11-18 17:44:41 -0300

    Tabs errors

--- music_cog.py
+++ music_cog.py
@@ -92,20 +92,20 @@ class music_cog(commands.Cog):
     @commands.command(name="pause", help="Pauses the current song being played")
     async def pause(self, ctx, *args):
         if self.is_playing:
-            self.is_playing = False
-            self.is_paused = True
-            self.vc.pause()
+		    self.is_playing = False
+		    self.is_paused = True
+		    self.vc.pause()
         elif self.is_paused:
-            self.is_paused = False
-			self.is_playing = True
-            self.vc.resume()
+		self.is_paused = False
+		self.is_playing = True
+		self.vc.resume()
 
     @commands.command(name = "resume", aliases=["r"], help="Resumes playing with the discord bot")
     async def resume(self, ctx, *args):
         if self.is_paused:
-            self.is_paused = False
-			self.is_playing = True
-            self.vc.resume()
+		self.is_paused = False
+		self.is_playing = True
+		self.vc.resume()
 
     @commands.command(name="skip", aliases=["s"], help="Skips the current song being played")
     async def skip(self, ctx):
